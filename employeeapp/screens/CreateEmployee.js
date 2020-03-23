1e8c3b3f59559bb0329abdcf7c9881ea186bba7e
shaheerqureshi <shaheerqureshi334@gmail.com>
2020-03-23 21:25:27 +0500

    Uploaded Images on Cloudinary

--- employeeapp/screens/CreateEmployee.js
+++ employeeapp/screens/CreateEmployee.js
@@ -1,6 +1,9 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View,Modal } from 'react-native';
+import { StyleSheet, Text, View,Modal,Alert } from 'react-native';
 import {TextInput,Button} from 'react-native-paper'
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
 
 const CreateEmployee = () =>{
     const [Name,setName] = useState("")
@@ -9,6 +12,65 @@ const CreateEmployee = () =>{
     const [salary,setsalary] = useState("")
     const [pic,setPicture] = useState("")
     const [modal,setModal] = useState(false)
+
+
+    const pickFromGallery = async () =>{
+       const{granted} = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+       if (granted) {
+          let data = await ImagePicker.launchImageLibraryAsync({
+               mediaTypes:ImagePicker.MediaTypeOptions.Images,
+               allowsEditing:true,
+               aspect:[1,1],
+               quality:0.5
+           })
+           if (!data.cancelled) {
+            let newfile = { 
+                uri:data.uri,
+                type:`test/${data.uri.split(".")[1]}`,
+                name:`test.${data.uri.split(".")[1]}` }
+                 handleUpload(newfile)
+           
+        }
+        
+       }else{
+            Alert.alert("You need to give us permission to work")
+       }
+    }
+    const pickFromCamera = async () =>{
+       const{granted} = await Permissions.askAsync(Permissions.CAMERA)
+       if (granted) {
+          let data = await ImagePicker.launchCameraAsync({
+               mediaTypes:ImagePicker.MediaTypeOptions.Images,
+               allowsEditing:true,
+               aspect:[1,1],
+               quality:0.5
+
+           })
+           if (!data.cancelled) {
+            let newfile = {uri:data.uri,type:`test/${data.uri.split(".")[1]}`,name:`test.${data.uri.split('.')[1]}`}
+            handleUpload(newfile)
+        }
+        
+       }else{
+            Alert.alert("You need to give us permission to work")
+       }
+    }
+    const handleUpload=(image)=>{
+        const data = new FormData()
+        data.append('upload_preset','employeeApp')
+        data.append('file',image)
+        data.append('cloud_name','shaheerqureshi')
+
+        fetch("https://api.cloudinary.com/v1_1/shaheerqureshi/image/upload",{
+            method:'post',
+            body:data
+        }).then(res=>res.json()).
+        then(data=>{
+            console.log(data)
+            setPicture(data.url)
+            setModal(false)
+        })
+    }
     return(
         <View style={StyleSheet.root}>
             <TextInput
@@ -47,7 +109,7 @@ const CreateEmployee = () =>{
             />
             <Button
             style={styles.modalButtonView}
-                icon="upload"
+                icon={pic == ""?"upload" :"check"}
                 mode='contained'
                 theme = {theme}
                 onPress={()=>setModal(true)}
@@ -71,8 +133,8 @@ const CreateEmployee = () =>{
                 >
                     <View style = {styles.modalView}>
                         <View style={styles.modalButtonView}>
-                        <Button  theme = {theme} icon="camera" mode='contained' onPress={()=>console.log("pressed")}>Camera</Button>        
-                        <Button  theme = {theme} icon="image-area" mode='contained' onPress={()=>console.log("pressed")}>Gallery</Button>
+                        <Button  theme = {theme} icon="camera" mode='contained' onPress={()=>pickFromCamera()}>Camera</Button>        
+                        <Button  theme = {theme} icon="image-area" mode='contained' onPress={()=>pickFromGallery()}>Gallery</Button>
                         
                         </View>
                     <Button icon="cancel" onPress={()=>setModal(false)}>Cancel</Button>
